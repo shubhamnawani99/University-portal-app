78ae6eedd6c985b54b53b3a75c3761295a9c9011
0xdeadsec <60750009+0xdeadsec@users.noreply.github.com>
2021-06-14 10:59:03 +0530

    Update AdminDAO.java

--- University Portal Workspace/universityportal/src/main/java/com/universityportal/database/dao/AdminDAO.java
+++ University Portal Workspace/universityportal/src/main/java/com/universityportal/database/dao/AdminDAO.java
@@ -22,44 +22,47 @@ import com.universityportal.utils.Log;
  *
  */
 public class AdminDAO {
-	
+
 	private Connection conn;
 	private PreparedStatement statement;
 	private static final ResourceBundle bundle = ResourceBundle.getBundle("Bundle");
-	
+
 	public boolean insertRecord(ArrayList<Marks> list) throws DatabaseException, FileReadException {
 
 		try {
 			conn = DB.getInstance().getConnection();
 			int confirm = 0;
-			int confirm1=0;
+			int confirm1 = 0;
+			if (list.isEmpty()) {
+				return false;
+			}
+
 			// to insert into marks table
-			for(Marks obj: list) {
-				
+			for (Marks obj : list) {
+
 				String query = "Insert into marks (semester,marks,subject_id) values (?,?,(select subject_id from subject where subject_id = ?));";
 				statement = conn.prepareStatement(query);
-				//statement.setInt(1, obj.getMarkID());
+				// statement.setInt(1, obj.getMarkID());
 				statement.setInt(1, obj.getSems());
 				statement.setInt(2, obj.getMarks());
 				statement.setString(3, obj.getSubjectID());
-				confirm=statement.executeUpdate();
-				
+				confirm = statement.executeUpdate();
+
 			}
 
 			// to insert into marks enrollment
-			for(Marks obj: list) {
-				
+			for (Marks obj : list) {
+
 				String sql = "Insert into marks_enrollment values ((select marks_id from marks where marks_id = ?),(select student_id from student where student_id = ?));";
 				statement = conn.prepareStatement(sql);
 				statement.setInt(1, obj.getMarkID());
 				statement.setString(2, obj.getStudentID());
-				confirm1=statement.executeUpdate();
-				
+				confirm1 = statement.executeUpdate();
+
 			}
-			if((confirm > 0) && (confirm1 > 0)) {
+			if ((confirm > 0) && (confirm1 > 0)) {
 				return true;
 			}
-			
 
 		} catch (SQLException e) {
 			Log.getLogger().log(Level.SEVERE, e.getMessage());
